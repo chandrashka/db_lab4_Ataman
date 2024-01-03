b107a87dd2f88b892ce4db691670de7d31514f06
Yuliia Ataman <47396774+chandrashka@users.noreply.github.com>
2024-01-03 12:27:16 +0100

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,14 +1,21 @@
+-- create.sql
+
+CREATE TABLE Artist (
+    artist_id SERIAL PRIMARY KEY,
+    artist_name VARCHAR(255)
+);
+
 CREATE TABLE Track (
     track_id SERIAL PRIMARY KEY,
     track_name VARCHAR(255),
-    artist_id INT REFERENCES Artist(artist_id),
-    artist_count INT,
-    released_year INT,
-    released_month INT,
-    released_day INT,
-    bpm INT,
-    key VARCHAR(255),
-    mode INT,
+    artist_count INTEGER,
+    artist_id INTEGER REFERENCES Artist(artist_id),
+    released_year INTEGER,
+    released_month INTEGER,
+    released_day INTEGER,
+    bpm INTEGER,
+    key INTEGER,
+    mode INTEGER,
     danceability_percent FLOAT,
     valence_percent FLOAT,
     energy_percent FLOAT,
@@ -18,30 +25,25 @@ CREATE TABLE Track (
     speechiness_percent FLOAT
 );
 
-CREATE TABLE Artist (
-    artist_id SERIAL PRIMARY KEY,
-    artist_name VARCHAR(255)
-);
-
 CREATE TABLE Playlist (
     playlist_id SERIAL PRIMARY KEY,
-    track_id INT REFERENCES Track(track_id),
-    in_spotify_playlists INT,
-    in_apple_playlists INT,
-    in_deezer_playlists INT
+    track_id INTEGER REFERENCES Track(track_id),
+    in_spotify_playlists INTEGER,
+    in_apple_playlists INTEGER,
+    in_deezer_playlists INTEGER
 );
 
 CREATE TABLE Charts (
-    charts_id SERIAL PRIMARY KEY,
-    track_id INT REFERENCES Track(track_id),
-    in_spotify_charts INT,
-    in_apple_charts INT,
-    in_deezer_charts INT,
-    in_shazam_charts INT
+    chart_id SERIAL PRIMARY KEY,
+    track_id INTEGER REFERENCES Track(track_id),
+    in_spotify_charts INTEGER,
+    in_apple_charts INTEGER,
+    in_deezer_charts INTEGER,
+    in_shazam_charts INTEGER
 );
 
 CREATE TABLE Streams (
-    streams_id SERIAL PRIMARY KEY,
-    track_id INT REFERENCES Track(track_id),
-    streams INT
+    stream_id SERIAL PRIMARY KEY,
+    track_id INTEGER REFERENCES Track(track_id),
+    streams INTEGER
 );
--- query.sql
+++ query.sql
@@ -1,25 +1,25 @@
--- Query 2a: Total streams for each track
-SELECT Track.track_name, SUM(Streams.streams) AS total_streams
+-- Query 2a: Sort streams
+SELECT Track.track_name, Streams.streams
 FROM Track
 JOIN Streams ON Track.track_id = Streams.track_id
-GROUP BY Track.track_name;
+ORDER BY Streams.streams DESC
 
--- Query 2b: Percentage of tracks in Spotify playlists
+-- Query 2b: Percentage of tracks in playlists that are higher than 50 place
 SELECT COUNT(Playlist.track_id) AS playlist_count, 'Spotify' AS platform
 FROM Playlist
-WHERE Playlist.in_spotify_playlists > 0
+WHERE Playlist.in_spotify_playlists > 50
 UNION
 SELECT COUNT(Playlist.track_id) AS playlist_count, 'Apple' AS platform
 FROM Playlist
-WHERE Playlist.in_apple_playlists > 0
+WHERE Playlist.in_apple_playlists > 50
 UNION
 SELECT COUNT(Playlist.track_id) AS playlist_count, 'Deezer' AS platform
 FROM Playlist
-WHERE Playlist.in_deezer_playlists > 0;
+WHERE Playlist.in_deezer_playlists > 50;
 
--- Query 2c: Number of tracks in Apple charts based on energy percentage
+-- Query 2c: Number of tracks in Spotify charts based on energy percentage that are higher than 100 place
 SELECT Track.energy_percent, COUNT(Charts.track_id) AS track_count
 FROM Track
 JOIN Charts ON Track.track_id = Charts.track_id
-WHERE Charts.in_apple_charts > 0
+WHERE Charts.in_spotify_charts < 100
 GROUP BY Track.energy_percent;
